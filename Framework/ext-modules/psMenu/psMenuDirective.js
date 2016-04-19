dc1fb775c4b121089fb70c7c66c65909e1d93945
Tommy Crafton <twcrafton@gmail.com>
2016-04-19 01:33:24 -0500

    Added files via upload

---
+++ Framework/ext-modules/psMenu/psMenuDirective.js
@@ -0,0 +1,19 @@
+﻿"use strict";
+
+angular.module('psMenu').directive('psMenu', ['$timeout', function ($timeout) {
+    return {
+        scope: {
+
+        },
+        transclude: true,
+        templateUrl: 'ext-modules/psMenu/psMenuTemplate.html',
+        controller: 'psMenuController',
+        link: function (scope, el, attr) {            
+            $timeout(function () {
+                var item = el.find('.ps-selectable-item:first');
+
+                item.trigger('click');
+            });
+        }
+    };
+}]);
